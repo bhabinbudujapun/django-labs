8dbd15955f175c7f7aaa758f86baa66c200dd1be
bhabinpun <poon.bhabin6332@gmail.com>
2026-05-05 19:11:49 +0545

    Add validation - title (min 6 chars), description (required, min 6 chars)

--- taskapi/tasks/models.py
+++ taskapi/tasks/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(6)])
+    description = models.TextField(blank=False, validators=[MinLengthValidator(6)])
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
